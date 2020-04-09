9fd21e184c153e80c07c5cc3ebc0c3c8e9d3cf47
peterboldyrev <53663056+peterboldyrev@users.noreply.github.com>
2020-04-08 22:47:15 -0700

    adding db file

--- Develop/public/db.js
+++ Develop/public/db.js
@@ -1,3 +1,5 @@
+import { response } from "express";
+
 let db;
 
 const request = window.indexedDB.open('workout',1);
@@ -23,21 +25,35 @@ request.onerror = function(event){
 
 
 function saveRecord(record){
-    const training = db.transaction(["pending"],"readwrite");
-    const store = transaction.ObjectStore("pending");
+    const training = db.training(["pending"],"readwrite");
+    const store = training.ObjectStore("pending");
     store.add(record);
 }
 
 function checkDatabase(){
     const training = db.transaction(["pending"], "readwrite");
-    const store = transaction.objectStore("pending");
+    const store = training.objectStore("pending");
     const getAll = store.getAll();
 
     getAll.onsuccess = function(){
         if(getAll.result.length>0){
-            fetch("")
+            fetch("/api/workouts", {
+                method: "POST",
+                body: JSON.stringify(data),
+                headers: { "Content-Type": "application/json" }
+              })
+              .then(response => response.json())
+                .then(()=>{
+                    const transaction = db.transaction(["pending"], "readwrite");
+                    // access your pending object store
+                const store = training.objectStore("pending");
+                      // clear all items in your store
+                store.clear();
+                })
         }
     }
 
 
 }
+// listen for app coming back online
+window.addEventListener("online", checkDatabase);
--- Develop/server.js
+++ Develop/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+var morgan = require('morgan');
 const path = require("path");
 
 const PORT = process.env.PORT || 8000;
@@ -8,10 +9,11 @@ const db = require("./models");
 
 const app = express();
 
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }));//if false then parse only strings
 app.use(express.json());
 
 app.use(express.static("public"));
+app.use(morgan('dev'));//log all the requests to the console
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
